6c1cc1231f813c8947e92a4b724fff9f7aa1eaab
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-16 12:34:06 -0400

    added javadoc and /index endpoint

--- java/com/MovieHunter/MovieHunterController.java
+++ java/com/MovieHunter/MovieHunterController.java
@@ -3,13 +3,25 @@ package com.MovieHunter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class MovieHunterController {
-
+	
+	
+	/** Handle the /start end point
+	 * @return
+	 */
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
 	}
 	
+	/** Handle the /index end point
+	 * @return
+	 */
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
 	
 }
