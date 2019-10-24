3cea031ca4fd386e31d8d68708c15090ef1dc050
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-24 15:12:24 -0400

    Added main to code

--- src/test/java/com/MovieHunter/MovieHunter/MovieHunterApplicationTests.java
+++ src/test/java/com/MovieHunter/MovieHunter/MovieHunterApplicationTests.java
@@ -1,9 +1,12 @@
 package com.MovieHunter.MovieHunter;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +15,15 @@ public class MovieHunterApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHunterApplicationTests.class, args);
+	
 }
+}
+
+
+
+
+
+
