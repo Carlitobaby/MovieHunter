2510c48407354d634fcc502a3dd6177551b46354
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-24 15:11:59 -0400

    Added some code from the in class labs

--- java/com/MovieHunter/MovieHunterController.java
+++ java/com/MovieHunter/MovieHunterController.java
@@ -2,6 +2,9 @@ package com.MovieHunter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MovieHunterController {
@@ -15,4 +18,19 @@ public class MovieHunterController {
 	//"Selection does not contain a main type.  I'm going to review the code from ours and
 	//see if I can add what you're missing to get your start page to run
 	
+
+	//This is where I'll begin importing the code 
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String start() {
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String create() {
+		return "start";
+	}
 }
+
+
+
