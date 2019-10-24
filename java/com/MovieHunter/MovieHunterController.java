a41068cc4df4c472fdf24a860131a49e8980ead1
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-24 14:46:00 -0400

    test push

--- java/com/MovieHunter/MovieHunterController.java
+++ java/com/MovieHunter/MovieHunterController.java
@@ -1,20 +1,27 @@
 package com.MovieHunter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//import com.MovieHunter.dto.MovieDTO;
+//import com.MovieHunter.service.IMovieService;
+
 
 @Controller
 public class MovieHunterController {
 	
+	@Autowired
+	//private IMovieService movieServiceStub;
 	
 	/** Handle the /start end point
 	 * @return
 	 */
 	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String read() {
+		//MovieDTO movieDTO = movieServiceStub.fetchById(12);
 		return "start";
 	}
 	
