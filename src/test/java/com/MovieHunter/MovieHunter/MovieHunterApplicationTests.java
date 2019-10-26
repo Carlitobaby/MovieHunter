a04d9bea0bd6c80109b4959fa9ad31b2379a6f36
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-25 20:38:49 -0400

    Removed the comments from this one too, to put in pull requests comments

--- src/test/java/com/MovieHunter/MovieHunter/MovieHunterApplicationTests.java
+++ src/test/java/com/MovieHunter/MovieHunter/MovieHunterApplicationTests.java
@@ -15,19 +15,13 @@ public class MovieHunterApplicationTests {
 	public void contextLoads() {
 	}
 	
-	//It's also important to have a main method with a .run command to get the spring
-	//to work.  This I mentioned this earlier but the error I got when I ran your code
-	//said you didn't have a main method, so it's important to add one.
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieHunterApplicationTests.class, args);
 	
 }
 }
-
-//now, after I ran that after adding the code and choosing to run the movie 
-//application.test file it's giving me a runtime error, but the previous error 
-//of not having a main method no longer pops up, so that's good. 
 
 
 
