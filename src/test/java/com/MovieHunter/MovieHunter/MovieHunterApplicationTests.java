43d37c9d34eb538dbdfc431fe9ccb7fb7dd2104d
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-25 20:31:18 -0400

    I decided to remove a commented out import package I had origionally added because it wasn't being used.

--- src/test/java/com/MovieHunter/MovieHunter/MovieHunterApplicationTests.java
+++ src/test/java/com/MovieHunter/MovieHunter/MovieHunterApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
