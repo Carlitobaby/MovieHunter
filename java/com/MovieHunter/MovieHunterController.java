f7b2a1331106d98f0c273203318e0fff25790227
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-25 20:37:54 -0400

    Removed comment and added the text from the comment to a document
    so I can add the text to the comments for the pull request

--- java/com/MovieHunter/MovieHunterController.java
+++ java/com/MovieHunter/MovieHunterController.java
@@ -14,16 +14,7 @@ public class MovieHunterController {
 		return "start";
 	}
 	
-//When attempting to debug and run the application it says that the
-	//"Selection does not contain a main type.  I'm going to review the code from ours and
-	//see if I can add what you're missing to get your start page to run
-	
 
-	//This is where I'll begin importing the code I've added a second @request mapping value to 
-	//the code that has method=RequestMethod.Get within it.  I beleive that might be why the 
-	//start.html page isn't working.  I've also added a post mapping string create that might also help.
-	//Having a blank @requestMapping method won't give you any results since it's not calling a .GET
-	//method. 
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String start() {
